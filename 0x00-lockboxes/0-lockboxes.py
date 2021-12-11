56950fa536ffa72a3a907e0049d90d51751a0e9d
mareizaa <marce.3210@gmail.com>
2021-12-11 08:03:33 -0500

    task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,14 +4,14 @@
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened."""
-    opened= [0];
+    opened = [0]
 
     for key in opened:
         for b in boxes[key]:
             if b < len(boxes) and b not in opened:
-                opened.append(b);
+                opened.append(b)
 
     if len(boxes) == len(opened):
-        return True;
+        return True
     else:
-        return False;
+        return False
