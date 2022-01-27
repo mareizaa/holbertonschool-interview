04342839b2aa8d2806b7db6685e8f1fb4a12fa55
mareizaa <marce.3210@gmail.com>
2022-01-27 12:25:29 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,45 +4,50 @@ Reads stdin line by line and computes metrics
 """
 from sys import stdin
 
-if __name__ == "__main__":
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+reponse_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    size = 0
-
-
-    def print_stats():
-        """Prints logs"""
-        print("File size: {}".format(size))
-        for status in sorted(status_codes.keys()):
-            if status_codes[status]:
-                print("{}: {}".format(status, status_codes[status]))
+t_size = {'size': 0}
 
 
+def print_reponse():
+    """Prints logs"""
+    print("File size: {}".format(t_size['size']))
+    for code in sorted(reponse_codes.keys()):
+        if reponse_codes[code] > 0:
+            print("{}: {}".format(code, reponse_codes[code]))
 
-    count = 0
+
+
+def result(item):
+    """Operates with the resume stats"""
+    try:
+        item = item.split(' ')
+        size = item[-1]
+        t_size["size"] += int(size)
+        if item[-2] in reponse_codes:
+            reponse_codes[item[-2]] += 1
+    except Exception as e:
+        pass
+
+
+if __name__ == '__main__':
+    num_lines = 1
     try:
         for line in stdin:
-            try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
-            except:
-                pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
+            result(line)
+            if num_lines % 10 == 0:
+                print_reponse()
+            num_lines += 1
     except KeyboardInterrupt:
-        print_stats()
+        print_reponse()
         raise
-    print_stats()
+    print_reponse()
