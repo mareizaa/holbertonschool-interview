534b67321afb9f4d2d887a372f3969ae68392413
mareizaa <marce.3210@gmail.com>
2022-01-27 11:51:34 -0500

    Update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,29 +4,30 @@ Reads stdin line by line and computes metrics
 """
 from sys import stdin
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-size = 0
-
-
-def print_stats():
-    """Prints logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
-
-
 if __name__ == "__main__":
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+
+    size = 0
+
+
+    def print_stats():
+        """Prints logs"""
+        print("File size: {}".format(size))
+        for status in sorted(status_codes.keys()):
+            if status_codes[status]:
+                print("{}: {}".format(status, status_codes[status]))
+
+
+
     count = 0
     try:
         for line in stdin:
