8371d989eb935d2d8d24dae7688c2ab1692973d9
mareizaa <marce.3210@gmail.com>
2022-01-27 12:32:45 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,59 +1,47 @@
 #!/usr/bin/python3
-
 """
 Reads stdin line by line and computes metrics
 """
+from sys import stdin
 
 
-import sys
-
-
-total_file_size = {'size': 0}
-codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
+response_code = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
 
-
-def print_data():
-    """
-        Prints logs
-    """
-    print('File size: {}'.format(total_file_size['size']))
-    for key in sorted(codes.keys()):
-        if codes[key] > 0:
-            print('{}: {}'.format(key, codes[key]))
+size = 0
 
 
-def operate_resume(line):
-    """
-        Operates with the resume stats
-    """
+def print_response():
+    """Prints logs"""
+    print("File size: {}".format(size))
+    for status in sorted(response_code.keys()):
+        if response_code[status]:
+            print("{}: {}".format(status, response_code[status]))
+
+if __name__ == "__main__":
+    count = 0
     try:
-        line = line.split(' ')
-        size = line[-1]
-        total_file_size['size'] += int(size)
-        if line[-2] in codes:
-            codes[line[-2]] += 1
-    except Exception as e:
-        pass
-
-
-if __name__ == '__main__':
-    num_lines = 1
-    try:
-        for line in sys.stdin:
-            operate_resume(line)
-            if num_lines % 10 == 0:
-                print_data()
-            num_lines += 1
+        for line in stdin:
+            try:
+                items = line.split()
+                size += int(items[-1])
+                if items[-2] in response_code:
+                    response_code[items[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                print_response()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print_data()
+        print_response()
         raise
-    print_data()
+    print_response()
