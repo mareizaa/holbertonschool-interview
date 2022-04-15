cac6364855b3f0c417a96bfa91aa88affa103598
mareizaa <marce.3210@gmail.com>
2022-04-15 18:50:55 -0500

    Code style

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Given a list of non-negative integers representing the heights of walls with unit width 1,
-as if viewing the cross-section of a relief map, calculate how many square units of water
+Given a list of non-negative integers representing
+the heights of walls with unit width 1,
+as if viewing the cross-section of a relief map,
+calculate how many square units of water
 will be retained after it rains.
 """
 
